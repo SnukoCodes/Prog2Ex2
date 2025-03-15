a9382381c7aed1b56e3246dbad2b33cc0de6ad49
Corinna <corinna.jaeger@stud.fh-campuswien.ac.at>
2025-03-15 10:11:14 +0100

    Add JSON annotations to Movie class

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -1,18 +1,37 @@
 package at.ac.fhcampuswien.fhmdb.models;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Movie {
-    private final String title;
-    private final String description;
-    private final List<Genre> genres;
+    public String id;
+    public String title;
+    public Genre[] genres;
+    public int releaseYear;
+    public String description;
+    public String imgUrl;
+    public int lengthInMinutes;
+    public String [] directors;
+    public String [] writers;
+    public String [] mainCast;
+    public double rating;
 
-    public Movie(String title, String description, List<Genre> genres) {
+
+
+    public Movie(String id, String title,Genre[] genres,int releaseYear, String description, String imgUrl, int lengthInMinutes, String[] directors, String[] writers, String [] mainCast, double rating) {
+        this.id = id;
         this.title = title;
-        this.description = description;
         this.genres = genres;
+        this.releaseYear = releaseYear;
+        this.description = description;
+        this.imgUrl = imgUrl;
+        this.lengthInMinutes = lengthInMinutes;
+        this.directors = directors;
+        this.writers = writers;
+        this.mainCast = mainCast;
+        this.rating =  rating;
     }
 
     @Override
@@ -29,40 +48,37 @@ public class Movie {
         return this.title.equals(other.title) && this.description.equals(other.description) && this.genres.equals(other.genres);
     }
 
+    public String getId() {return id;}
+
     public String getTitle() {
         return title;
     }
+
+    public Genre[] getGenres() {
+        return genres;
+    }
+
+    public Integer getReleaseYear() {return releaseYear;}
 
     public String getDescription() {
         return description;
     }
 
-    public List<Genre> getGenres() {
-        return genres;
-    }
+    public String getImgUrl() {return imgUrl;}
 
-    public static List<Movie> initializeMovies(){
+    public Integer getLengthInMinutes() {return lengthInMinutes;}
+
+    public String[] getDirectors() {return directors;}
+
+    public String[] getWriters() {return writers;}
+
+    public String[] getMainCast() {return mainCast;}
+
+    public Number getRating() {return rating;}
+
+    public static List<Movie> initializeMovies() throws IOException {
         List<Movie> movies = new ArrayList<>();
-        movies.add(new Movie(
-                "Life Is Beautiful",
-                "When an open-minded Jewish librarian and his son become victims of the Holocaust, he uses a perfect mixture of will, humor, and imagination to protect his son from the dangers around their camp." ,
-                Arrays.asList(Genre.DRAMA, Genre.ROMANCE)));
-        movies.add(new Movie(
-                "The Usual Suspects",
-                "A sole survivor tells of the twisty events leading up to a horrific gun battle on a boat, which begin when five criminals meet at a seemingly random police lineup.",
-                Arrays.asList(Genre.CRIME, Genre.DRAMA, Genre.MYSTERY)));
-        movies.add(new Movie(
-                "Puss in Boots",
-                "An outlaw cat, his childhood egg-friend, and a seductive thief kitty set out in search for the eggs of the fabled Golden Goose to clear his name, restore his lost honor, and regain the trust of his mother and town.",
-                Arrays.asList(Genre.COMEDY, Genre.FAMILY, Genre.ANIMATION)));
-        movies.add(new Movie(
-                "Avatar",
-                "A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
-                Arrays.asList(Genre.ANIMATION, Genre.DRAMA, Genre.ACTION)));
-        movies.add(new Movie(
-                "The Wolf of Wall Street",
-                "Based on the true story of Jordan Belfort, from his rise to a wealthy stock-broker living the high life to his fall involving crime, corruption and the federal government.",
-                Arrays.asList(Genre.DRAMA, Genre.ROMANCE, Genre.BIOGRAPHY)));
+//        movies = MovieAPI.getMovies();
 
         return movies;
     }
